65b6ca8634bb3164600eb1d90fb8e4da405a2f7d
Craig Rupp <craigtrupp8@gmail.com>
2023-11-01 13:46:44 -0500

    Markdown for assignment and working code for countryInfo method with various tests

--- OOP_Specialization/SolvingProblemsWSoftware1_4/ThirdWeek/Parsing_Export_I/InitialCSVParser.java
+++ OOP_Specialization/SolvingProblemsWSoftware1_4/ThirdWeek/Parsing_Export_I/InitialCSVParser.java
@@ -8,6 +8,9 @@
 
 import edu.duke.*;
 import org.apache.commons.csv.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class InitialCSVParser {
     public void fileReader() throws java.io.IOException {
@@ -27,5 +30,42 @@ public class InitialCSVParser {
             }
         }
     }
-    
+    public String countryInfo(CSVParser parser, String country) throws java.io.IOException {
+        System.out.println(String.format("Passed country : %s", country));
+        List<String> countries = new ArrayList<String>();
+        List<CSVRecord> records = parser.getRecords(); // need to save records in order to iterate over multiple times (parser clears once after iterating)
+        for(CSVRecord record : records){
+            countries.add(record.get("Country").toLowerCase());
+        }
+        if(countries.contains(country.toLowerCase())){
+            String country_lc = country.toLowerCase();
+            for(CSVRecord record : records){
+                String recordCountry = record.get("Country").toLowerCase();
+                if(recordCountry.equals(country_lc)){
+                    return String.format("%s: %s: %s", record.get("Country"), record.get("Exports"), record.get("Value (dollars)"));
+                }
+            }
+            
+        }
+        return "NOT FOUND";
+    }
+    public void tester() throws java.io.IOException {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser(); // default is true (so header included)
+        // first all for countryInfo test (be sure to pass a new instance of parser for each method - after read the file is blank)
+        System.out.println(countryInfo(parser, "Germany")); // Germany: motor vehicles, machinery, chemicals: $1,547,000,000,000
+        CSVParser secondParser = fr.getCSVParser();
+        System.out.println(countryInfo(secondParser, "SOUth AfRICA"));
+        secondParser = fr.getCSVParser();
+        System.out.println(countryInfo(secondParser, "Antartica"));
+        /*
+        Passed country : Germany
+        Germany: motor vehicles, machinery, chemicals: $1,547,000,000,000
+        Passed country : SOUth AfRICA
+        South Africa: gold, diamonds, platinum: $97,900,000,000
+        Passed country : Antartica
+        NOT FOUND
+        */
+        
+    }
 }
---
+++ OOP_Specialization/SolvingProblemsWSoftware1_4/ThirdWeek/Parsing_Export_I/Parsing_Export_Ex.md
@@ -0,0 +1,16 @@
+## Programming Exercise : Parsing Export Data 🚢
+The CSV file `exportdata.csv` has information on the export products of countries; you can download a .zip folder with this and other export data files. In particular it has three column headers labeled `Country, Exports, and Value (dollars)`. 
+* The Country column represents a country from the world, 
+* the Exports column is a list of export items for a country, and 
+* the Value (dollars) column is the dollar amount in millions of their exports in the format of a dollar sign, followed by an integer number with a comma separator every three digits from the right. An example of such a value might be “$400,000,000”.
+
+The CSV file `exports_small.csv` is a smaller version of the file above with the same columns that you may find helpful in testing your program. 
+
+### `Task`
+1. Write a method named **tester** that will create your `CSVParser` and call each of the methods below in parts 2,3,4, and 5.
+    * Each time you want to use the parser with another method, you will need to reset the parser by calling fr.getCSVParser() again to get a new parser.
+
+2. Write a method named `countryInfo` that has two parameters, `parser is a CSVParser and country is a String`. 
+    * This method returns a string of information about the country or returns “NOT FOUND” if there is no information about the country. 
+    * The format of the string returned is the country, followed by “: “, followed by a list of the countries’ exports, followed by “: “, followed by the countries export value. For example, using the file exports_small.csv and the country Germany, the program returns the string:
+        - Germany: motor vehicles, machinery, chemicals: $1,547,000,000,000
